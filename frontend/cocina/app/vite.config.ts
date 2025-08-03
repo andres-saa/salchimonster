3a9debb3ccd16a5f72cff1883b04bcb33e17904f
andres-saa <andres19f@gmail.com>
2025-08-03 23:18:48 +0100

    correccion host

--- frontend/cocina/app/vite.config.ts
+++ frontend/cocina/app/vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
     allowedHosts: [
       'localhost',
       '127.0.0.1',
+      'cocina.docker.salchimonster.com',
       'cocina.127.0.0.1.nip.io',   // ← el que necesitas
     ],
 
